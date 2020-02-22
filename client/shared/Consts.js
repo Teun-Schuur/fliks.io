ecc645b7ade1160a0feba6aa131bd7ca042066cf
Teun Schuur <teun983@gmail.com>
2020-02-22 22:37:31 +0100

    to shoot you have to pay with your score

--- client/shared/Consts.js
+++ client/shared/Consts.js
@@ -1,27 +1,29 @@
 const consts = Object.freeze({
-  MAP_WIDTH: 2400,
-  MAP_HEIGHT: 2400,
+  MAP_WIDTH: 2200,
+  MAP_HEIGHT: 2200,
   BACKGROUND: [35, 43, 43],
   FRAME_RATE: 60,
+  NIGHT: true,
 
   FOOD_RADIUS: 10,
 
   PLAYER_HP: 100,
-  PLAYER_SIZE: 33,
+  PLAYER_SIZE: 40,
   PLAYER_SHOOTING_SPEED: 0.1,
 
   BEGIN_HP: 100,
-  FOOD_SCORE: 1,
+  FOOD_SCORE: 7,
   FOOD_RESPAN_RATE: 1,
-  MAX_FOOD_SPAN: 1000,
+  MAX_FOOD_SPAN: 30,
 
-  BULLET_SPEED: 16,
+  BULLET_SPEED: 13,
+  BULLET_COST: 3,
 
   VIEW_CIRCLE_MIN: 200,
-  VIEW_CIRCLE_MAX: 450,
+  VIEW_CIRCLE_MAX: 900,
 
   COLORS: {
-    bullet: [200, 200, 255],
+    bullet: [192, 192, 192],
     obstical: [200, 0, 210],
     player_self: [167, 229, 65],
     food: [255, 100, 0]
